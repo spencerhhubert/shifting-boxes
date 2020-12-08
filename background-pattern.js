9a530e3b5b750aee7f187fd3f4d67d1a381d5e33
spencerhhubert <spencerhhubert@gmail.com>
2020-12-08 11:49:51 -0800

    opacity decrease with distance from origin and code for random moving that doesn't work

--- background-pattern.js
+++ background-pattern.js
@@ -13,6 +13,12 @@ function getRGBValueInRange(top, bottom) {
     return Math.floor(Math.random()*(top-bottom)+bottom)
 }
 
+function rawAttributeValue(attribute) {
+    let result = attribute.slice(0,-2)
+    result = parseInt(result, 10)
+    return result
+}
+
 function randomPink() {
     let value1 = getRGBValueInRange(220, 255);
     let value2 = getRGBValueInRange(20, 90);
@@ -41,6 +47,8 @@ function generatePixel() {
     pixel.verticalMax = 16 * pixel.squareSize;
     pixel.xCord = Math.floor(boxMullerPositiveFloat()*pixel.horizontalMax/pixel.squareSize) * pixel.squareSize;
     pixel.yCord = Math.floor(boxMullerPositiveFloat()*pixel.verticalMax/pixel.squareSize) * pixel.squareSize;
+    pixel.originDistance = Math.sqrt(pixel.xCord**2 + pixel.yCord**2)
+    console.log(pixel.originDistance);
     pixel.color = randomPink();
     return pixel
 }
@@ -52,10 +60,11 @@ for(x=0; x<totalNumberOfPixels; x++) {
     pixelNode = document.createElement('div');
     pixelNode.className = 'pixel'
     pixelNode.style.backgroundColor = newPixel.color;
-    pixelNode.style.left = newPixel.xCord + "px";
-    pixelNode.style.top = newPixel.yCord + "px";
+    pixelNode.style.left = newPixel.xCord;
+    pixelNode.style.top = newPixel.yCord;
     pixelNode.style.width = newPixel.squareSize + "px";
     pixelNode.style.height = newPixel.squareSize + "px";
+    pixelNode.style.opacity = 1/(newPixel.originDistance/300);
     let currentZIndex = 2;
 
     pixelNode.addEventListener("mouseenter", function(event) {
@@ -69,6 +78,34 @@ for(x=0; x<totalNumberOfPixels; x++) {
 
     pixelNode.addEventListener("mousedown", function(event) {
         event.target.remove()
+        movePixelRandomWay(pixelNode, newPixel)
     })
+
+    // pixelNode.addEventListener(setInterval(1000), function(event) {
+    //     if (Math.random() < .5) {
+    //         event.target.style.top = rawAttributeValue(event.target.style.top) + newPixel.squareSize;
+    //     }
+    // })
+
+    // movePixelRandomWay(pixelNode, newPixel);
+
     document.getElementById('pixel-drip').appendChild(pixelNode)
+}
+
+function movePixelRandomWay(node, object) {
+    let randomValue = Math.random();
+    switch(randomValue) {
+        case randomValue < .25:
+            node.style.left = rawAttributeValue(node.style.left) + object.squareSize;
+            break;
+        case randomValue > .25 && randomValue < .5:
+            node.style.top = rawAttributeValue(node.style.top) + object.squareSize;
+            break;
+        case randomValue > .5 && randomValue < .75:
+            node.style.right = rawAttributeValue(node.style.right) + object.squareSize;
+            break;
+        case randomValue > .75 && randomValue < 1:
+            node.style.top = rawAttributeValue(node.style.top) + object.squareSize;
+            break;
+    }
 }
